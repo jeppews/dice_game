bdebf97c5f73963c4ee0c6efa29d1e13f8a92ba1
RasmusRow <rasmus.rowbotham@gmail.com>
2015-03-23 10:46:31 +0100

    Nu virker det, pikhoveder

--- index.php
+++ index.php
@@ -8,7 +8,16 @@
             <?php 
                 $rand = rand(1,6);
                 
-                echo 'You rolled a: '.$rand;
+                if($rand == 1) : $rand = 'uno';
+                elseif($rand == 2) : $rand = 'dos';
+                elseif($rand == 3) : $rand = 'tres';
+                elseif($rand == 4) : $rand = 'quatro';
+                elseif($rand == 5) : $rand = 'cinco';
+                elseif($rand == 6) : $rand = 'seis';
+                
+                endif;
+                
+                echo 'You rolled a: <img src="img/'.$rand.'.jpg" />';
             ?>
     </body>
 </html>
