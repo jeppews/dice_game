1e69c97923e8944bff0d274a870fa777547fd573
RasmusRow <rasmus.rowbotham@gmail.com>
2015-03-23 10:33:30 +0100

    Yo

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>wat</title>
-</head>
-<body>
-	<h1>ssHeyyy</h1>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>wat</title>
+    </head>
+    <body>
+            <?php 
+                $rand = rand(1,6);
+                
+                echo $rand;
+            ?>
+    </body>
 </html>
